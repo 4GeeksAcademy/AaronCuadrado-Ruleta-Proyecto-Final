a931ccb4cd22898cab9a992a5caaa13a0cd43eb2
Aaron Cuadrado <163054420+AaronCuadrado@users.noreply.github.com>
2024-10-23 21:23:55 +0000

    home no me funciona

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,25 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/home.css"; 
+import logo1 from "../img/logo1.png"
 
-export const Home = () => {
-	const { store, actions } = useContext(Context);
-
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="overlay">
+        <img src={logo1} alt="Casino Logo" className="logo" />
+        <h1>¡Bienvenido al Casino!</h1>
+        <div className="buttons-container">
+          <Link to="/register" className="btn register-btn">
+            Registro
+          </Link>
+          <Link to="/login" className="btn login-btn">
+            Inicio de Sesión
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default Home;
