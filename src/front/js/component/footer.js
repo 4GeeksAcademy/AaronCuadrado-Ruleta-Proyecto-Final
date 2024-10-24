e8583c15692c5f468d31952f44f221017f78cadb
Aaron Cuadrado <163054420+AaronCuadrado@users.noreply.github.com>
2024-10-24 13:36:11 +0000

    footer completado

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
+import "../../styles/footer.css"; // Asegúrate de que la ruta a tu archivo CSS es correcta
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer>
+		<div className="footer-container">
+			<div className="footer-column">
+				<h4>Contacto</h4>
+				<p>email@ruletadorada.com</p>
+				<p>Tel: +34 112 112 112</p><br></br>
+			</div>
+
+			<div className="footer-column">
+				<h4>Redes Sociales</h4>
+				<div className="social-icons">
+					<a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
+					<a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+					<a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+				</div><br></br>
+			</div>
+
+			<div className="footer-column">
+				<h4>Dirección</h4>
+				<p>Calle no se me ocurre, Barakaldo, Bizkaia</p>
+			</div><br></br>
+		</div>
+		<div className="copyright">
+			&copy; 2024 Hecho por Aaron Cuadrado. Todos los derechos reservados.
+		</div>
 	</footer>
 );
+
