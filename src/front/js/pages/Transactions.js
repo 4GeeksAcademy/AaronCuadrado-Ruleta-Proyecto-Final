d020c6e6536440b97e710f2681e36909ff127d4c
Aaron Cuadrado <163054420+AaronCuadrado@users.noreply.github.com>
2024-10-29 13:50:05 +0000

    pagina transacciones mejorada, pero sin conectar a la api

--- src/front/js/pages/Transactions.js
+++ src/front/js/pages/Transactions.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Navbar } from "../component/Navbar";
-import { Footer } from "../component/footer";
 import "../../styles/transactions.css";
 import logo1 from "../../img/logo1.png";
 
@@ -16,23 +15,27 @@ export const Transactions = () => {
         <div className="transactions-page">
             <Navbar />
             <div className="transactions-content">
-                <div className="transactions-logo">
-                    <img src={logo1} alt="logo" />
-                </div>
-                <div className="transactions-categories">
-                    <select value={category} onChange={handleCategoryChange}>
-                        <option value="">Todas las categorias</option>
-                        <option value="bet">Apuestas</option>
-                        <option value="deposit">Ingresos</option>
-                        <option value="withdraw">Retiradas</option>
-                    </select>
+                <div className="transactions-inner-container">
+                    <div className="transactions-logo">
+                        <img src={logo1} alt="logo" />
+                    </div>
+                    <div className="transactions-categories">
+                        <select value={category} onChange={handleCategoryChange}>
+                            <option value="">Todas las categorias</option>
+                            <option value="bet">Apuestas</option>
+                            <option value="deposit">Ingresos</option>
+                            <option value="withdraw">Retiradas</option>
+                        </select>
+                    </div>
+
+
+                    <div className="transactions-list">
+                        <div className="transactions-item">
+                            <p>ejemplo de transaccion</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div className="transactions-list">
-
-            </div>
-            <Footer />
         </div>
     );
 };
