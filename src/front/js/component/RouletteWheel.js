b064d4f2e7d28fb7c05f8e3ce634e6c2cce8e4b6
Aaron Cuadrado <163054420+AaronCuadrado@users.noreply.github.com>
2024-11-10 21:59:02 +0000

    mejora de wheel

--- src/front/js/component/RouletteWheel.js
+++ src/front/js/component/RouletteWheel.js
@@ -1,14 +1,60 @@
 // src/component/RouletteWheel.js
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../../styles/roulette/RouletteWheel.css';
 
 const RouletteWheel = ({ spinAngle }) => {
+    const numbers = [
+        { number: 0, color: 'green' },
+        { number: 32, color: 'red' },
+        { number: 15, color: 'black' },
+        { number: 19, color: 'red' },
+        { number: 4, color: 'black' },
+        { number: 21, color: 'red' },
+        { number: 2, color: 'black' },
+        { number: 25, color: 'red' },
+        { number: 17, color: 'black' },
+        { number: 34, color: 'red' },
+        { number: 6, color: 'black' },
+        { number: 27, color: 'red' },
+        { number: 13, color: 'black' },
+        { number: 36, color: 'red' },
+        { number: 11, color: 'black' },
+        { number: 30, color: 'red' },
+        { number: 8, color: 'black' },
+        { number: 23, color: 'red' },
+        { number: 10, color: 'black' },
+        { number: 5, color: 'red' },
+        { number: 24, color: 'black' },
+        { number: 16, color: 'red' },
+        { number: 33, color: 'black' },
+        { number: 1, color: 'red' },
+        { number: 20, color: 'black' },
+        { number: 14, color: 'red' },
+        { number: 31, color: 'black' },
+        { number: 9, color: 'red' },
+        { number: 22, color: 'black' },
+        { number: 18, color: 'red' },
+        { number: 29, color: 'black' },
+        { number: 7, color: 'red' },
+        { number: 28, color: 'black' },
+        { number: 12, color: 'red' },
+        { number: 35, color: 'black' },
+        { number: 3, color: 'red' },
+        { number: 26, color: 'black' }
+    ];
+
     return (
         <div className="roulette-wheel-container">
             <div className="roulette-wheel" style={{ transform: `rotate(${spinAngle}deg)` }}>
-                {[...Array(37)].map((_, i) => (
-                    <div key={i} className={`roulette-number ${i % 2 === 0 ? 'black' : 'red'} ${i === 0 ? 'green' : ''}`} style={{ transform: `rotate(${i * 9.73}deg)` }}>
-                        {i}
+                {numbers.map((item, index) => (
+                    <div
+                        key={index}
+                        className={`roulette-number ${item.color}`}
+                        style={{
+                            transform: `rotate(${index * (360 / 37)}deg) translate(0, -130px)`,
+                        }}
+                    >
+                        {item.number}
                     </div>
                 ))}
             </div>
